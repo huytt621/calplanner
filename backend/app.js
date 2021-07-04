4d98a9ae0611682b458968278b23ccbbc6f4089a
Huy Tran <huytt621@gmail.com>
2021-07-03 18:40:27 -0700

    Build basic structure of backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,24 @@
+const config = require('./utils/config')
 const express = require('express')
+const app = express()
+const userRouter = require('./controllers/users')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/users', userRouter)
+
+module.exports = app
+
+
