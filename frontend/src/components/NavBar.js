361073f5473b289a5c874dc1a2f8a04c1077ca67
Huy Tran <huytt621@gmail.com>
2021-07-24 17:06:12 -0700

    Add basic styling to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,23 +2,25 @@ import { Link } from "react-router-dom"
 
 const NavBar = ({ user }) => {
   return (
-    <nav>
-      <ul>
+    <nav className="bg-berkeley-blue text-white">
+      <ul className="flex flex-row justify-between">
         <li>
-          <Link to="/">CalPlanner</Link>
+          <Link to="/" className="text-3xl p-2 ml-4">CalPlanner</Link>
         </li>
-        <li>
-          <Link to="/plans">Plans</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <div>{user ? user.username : ''}</div>
-        </li>
+        <div className="flex flex-row">
+          <li className="p-2">
+            <Link to="/plans">Plans</Link>
+          </li>
+          <li className="p-2">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="p-2">
+            <Link to="/register">Register</Link>
+          </li>
+          <li className="p-2 mr-4">
+            <div>{user ? user.username : ''}</div>
+          </li>
+        </div>
       </ul>
     </nav>
   )
