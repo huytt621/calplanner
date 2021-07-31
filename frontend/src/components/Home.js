e444e8a5601a9b0a2952d9dc5a0ebeb41e7f787b
Siddharth <marathesidd@gmail.com>
2021-07-30 22:52:37 -0700

    added styling to home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,25 +6,27 @@ const Home = ( {user} ) => {
 		return ('')
 	}
   	return (
-	  	<div>
-		  	<h1>
+	  	<div className="text-center text-2xl p-2 ml-4">
+		  	<h1 className="mx-4 bg-yellow-400 rounded-lg rounded-full py-3 px-6 text-4xl p-2 ml-4">
 		  		Welcome to CalPlanner{un(user)}!
 		  	</h1> 
-		  	<h1>
+		  	<div className="mx-52 bg-blue-400 rounded-lg rounded-full p-4 whitespace-pre-wrap">
 		  		The number one place to discuss class schedules and talk about your favorite courses!
-		  	</h1>
-		  	<h1>
-		  		Our Features
-		  	</h1>
-		  	<h2>
-		  		Class Schedules For All Majors
-		  	</h2>
-		  	<h2>
-		  		Dicussion Among Students
-		  	</h2>
-		  	<h2>
-		  		Popularity and Difficulty Meter 
-		  	</h2>
+		  	</div>
+		  	<div className="py-32 text-4xl p-2 ml-4">
+		  		Features
+		  	</div>
+			<div className="grid grid-cols-3 gap-2 place-items-center h-48">
+			  	<div className="rounded-full border-solid bg-green-400 w-9/12 h-8">
+			  		Class Schedules For All Majors
+			  	</div>
+			  	<div className="rounded-full border-solid bg-red-400 w-9/12 h-8">
+			  		Dicussion Among Students
+			  	</div>
+			  	<div className="rounded-full border-solid bg-blue-400 w-9/12 h-8">
+			  		Popularity and Difficulty Meter 
+			  	</div>
+		  	</div>
 		</div>
   	)
 }
