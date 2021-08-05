6773aae4f0afc637b88f31a518cf6c35d32f876a
Huy Tran <huytt621@gmail.com>
2021-08-04 19:58:30 -0700

    Add delete year buttons

--- frontend/src/components/Year.js
+++ frontend/src/components/Year.js
@@ -14,8 +14,16 @@ const Year = ({ year, yearIndex, plan, setPlan }) => {
       Cell: EditableCell
     }
   ]
+
+  const deleteYear = () => {
+    const newPlan = {...plan}
+    newPlan.years.splice(yearIndex, 1)
+    setPlan(newPlan)
+  }
+
   return (
     <div className="flex flex-row">
+      <button onClick={deleteYear}>Delete</button>
       {year.map((session, index) => <Session key={session.name} columns={[{ Header: session.name, columns: columns }]} data={session.courses} year={yearIndex} sessionIndex={index} plan={plan} setPlan={setPlan} />)}
     </div>
   )
