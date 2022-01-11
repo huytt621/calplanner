0e2621af28ee91ca73102dc25d4a77c0fc20d988
Huy Tran <huytt621@gmail.com>
2022-01-10 20:37:03 -0800

    Add route to check for authentication

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,12 +1,30 @@
 const passport = require('passport')
 const loginRouter = require('express').Router()
 
-loginRouter.get('/google', passport.authenticate('google', {
-  scope: ['profile']
-}))
+loginRouter.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile'],
+  })
+)
 
-loginRouter.get('/google/callback', passport.authenticate('google'), (request, response) => {
-  response.redirect('/')
+loginRouter.get(
+  '/google/callback',
+  passport.authenticate('google'),
+  (request, response) => {
+    response.redirect('/')
+  }
+)
+
+loginRouter.get('/authenticated', (request, response) => {
+  const user = request.user
+  if (!user) {
+    response.status(200).json({
+      authenticated: false,
+    })
+  } else {
+    response.status(200).json(user)
+  }
 })
 
 module.exports = loginRouter
